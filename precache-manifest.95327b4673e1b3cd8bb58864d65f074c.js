f74189f90c49c127da5454246b7ce7355ab7274e
stunstyle <stunstyle@yahoo.com>
2019-12-11 23:53:36 +0200

    Updates

---
+++ precache-manifest.95327b4673e1b3cd8bb58864d65f074c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbaafa9fb51d3269e119b66039b06342",
+    "url": "/thirsty-ui/index.html"
+  },
+  {
+    "revision": "3321c08a1815f6b7423c",
+    "url": "/thirsty-ui/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "30c2d09848b35df6a3a9",
+    "url": "/thirsty-ui/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3321c08a1815f6b7423c",
+    "url": "/thirsty-ui/static/js/2.97df5c14.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/thirsty-ui/static/js/2.97df5c14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "30c2d09848b35df6a3a9",
+    "url": "/thirsty-ui/static/js/main.97feb3f3.chunk.js"
+  },
+  {
+    "revision": "ba2fd43db68979ea8225",
+    "url": "/thirsty-ui/static/js/runtime-main.66876160.js"
+  }
+]);
--- src/components/About.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function About() {
-    return(
-        <div>kur</div>
-    );
-}
-export default About;
