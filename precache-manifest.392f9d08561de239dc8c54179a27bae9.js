443dfa99b9bb35bf4eca7e39ca0042b8e6b63617
stunstyle <stunstyle@yahoo.com>
2019-12-12 00:06:13 +0200

    Updates

---
+++ precache-manifest.392f9d08561de239dc8c54179a27bae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61116f95a365375c2c2a932e62cfaa69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3321c08a1815f6b7423c",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "48ef639defcef0b53a04",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "3321c08a1815f6b7423c",
+    "url": "/static/js/2.97df5c14.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.97df5c14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48ef639defcef0b53a04",
+    "url": "/static/js/main.285a2853.chunk.js"
+  },
+  {
+    "revision": "76793a7cf5ba3b5411a2",
+    "url": "/static/js/runtime-main.b6fd111f.js"
+  }
+]);
